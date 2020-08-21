42a0d12bc277eeeca67fafc48aa8d596878e67d6
hewen <812309203@qq.com>
2020-08-21 14:57:02 +0800

    把之前c

---
+++ 没啥用的c++/define.h
@@ -0,0 +1,15 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+//#define ACCOUNTLENGTH 20
+//#define PASSWORDLENGTH  20
+#define ANIMATIONTIME 5 //动画的速度
+#define WIDTH 600
+#define HEIGHT 400
+#define MAX 12
+#define BUTTONWIDTH 80
+#define BUTTONHEIGHT 20
+#define ICONWIDTH 160
+#define ICONHEIGHT  213
+
+#endif // DEFINE_H
